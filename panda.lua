c349a595a0b2455341504ef9c9091a4d70f84323
Alex Saalberg <saalberg.software@gmail.com>
2019-01-31 23:06:50 -0800

    Add panda class, add panda image

---
+++ panda.lua
@@ -0,0 +1,27 @@
+local Bear = require( "bear" )
+
+-- create the base table for the class definition
+local Panda = Bear:new()
+
+-- Panda static variables
+Panda.image = love.graphics.newImage("panda.png")
+Panda.radius = 20
+Panda.innerRadius = 10
+Panda.maxHealth = 100
+Panda.points = 1
+Panda.name = "panda"
+
+-- constructor
+function Panda:new()
+    -- create a new table for the object instance
+    local instance = {}
+
+    -- hook up its metatable to the class definition
+    setmetatable( instance, self )
+    self.__index = self
+
+    -- return the instance
+    return instance
+end
+
+return Panda
