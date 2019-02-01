593b664edb9d138d0a3c63c024d1c51151016c26
Alex Saalberg <saalberg.software@gmail.com>
2019-02-01 10:42:29 -0800

    Make EntityManager:delete() delete physics body. Add isAnimal() to EntityManager.

--- entityManager.lua
+++ entityManager.lua
@@ -16,13 +16,27 @@ function EntityManager:get(id)
 end
 
 function EntityManager:delete(id)
-  self.entities[id] = nil
+  if self.entities[id] then
+    if self.world and self.entities[id].body then
+      self.entities[id].body:destroy()
+    end
+    self.entities[id] = nil
+  end
 end
 
 function EntityManager:getNewId()
   local id = self.freshId
   self.freshId = id + 1
   return id
+end
+
+function EntityManager:isAnimal(id)
+  local entity = self.entities[id]
+  if entity and entity.name then
+    return true
+  end
+
+  return false
 end
 
 -- love callbacks
