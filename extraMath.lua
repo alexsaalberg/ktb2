8748de47e6d31c890ec117ca5dc9a9abb70f3cc9
Alex Saalberg <saalberg.software@gmail.com>
2019-02-04 14:39:27 -0800

    Add rbool method to ExtraMath. Returns true/false evenly.

--- extraMath.lua
+++ extraMath.lua
@@ -48,6 +48,8 @@ function ExtraMath.rsign() return love.math.random(2) == 2 and 1 or -1 end
  
 -- Returns 1 if number is positive, -1 if it's negative, or 0 if it's 0.
 function ExtraMath.sign(n) return n>0 and 1 or n<0 and -1 or 0 end
+
+function ExtraMath.rbool() return (love.math.random(3) == 1) end
  
 -- Gives a precise random decimal number given a minimum and maximum
 function ExtraMath.prandom(min, max) return love.math.random() * (max - min) + min end
